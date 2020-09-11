33ad2aca636e9c73b98637672df371cbb64af84c
vincent wittek# <wittek.vincent@gmail.com>
2020-09-12 00:32:57 +0200

    made a requests dashboard

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,10 +1,15 @@
 class RequestsController < ApplicationController
+
+  def index
+    @requests = policy_scope(Request).order(created_at: :desc)
+    # @requests = Request.where(user_id = current_user.id.to_s)
+  end
   def create
-      @request = Request.new
-      @event = Event.find(params[:event_id])
-      @request.event = @event
-      @request.user = current_user
-      authorize @request
+    @request = Request.new
+    @event = Event.find(params[:event_id])
+    @request.event = @event
+    @request.user = current_user
+    authorize @request
     
     if @request.save!
       redirect_to event_path(@event)
