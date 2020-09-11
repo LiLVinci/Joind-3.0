c7a19313bca283dc3e95d98639322cfcd8a14c55
vincent wittek# <wittek.vincent@gmail.com>
2020-09-12 00:08:51 +0200

    added a join! button with lets you join an event

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,15 @@
 class RequestsController < ApplicationController
+  def create
+      @request = Request.new
+      @event = Event.find(params[:event_id])
+      @request.event = @event
+      @request.user = current_user
+      authorize @request
+    
+    if @request.save!
+      redirect_to event_path(@event)
+    else
+      render :show
+    end
+  end
 end
